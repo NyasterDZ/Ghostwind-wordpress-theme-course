5af13413b64e4dce9a4e7ee02beb501c4cf919eb
Walid Hun <44478907+NyasterDZ@users.noreply.github.com>
2022-02-25 15:50:20 +0100

    #53

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form role="search"  method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <label for="search">Search for stuff</label>
   <input id="search" type="search"
-        placeholder="Search..."
-        value="<?php echo  esc_attr(get_search_query()); ?>" name="s"
+        placeholder="<?php echo  esc_attr('Search...') ?>"
+        value="<?php echo get_search_query(); ?>" name="s"
         autofocus required />
   <button type="submit">Go</button>    
 </form>
