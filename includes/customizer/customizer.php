447e456bdcd96c1e84b581c2b289503cf04e25f7
Walid Hun <44478907+NyasterDZ@users.noreply.github.com>
2022-03-16 20:06:38 +0100

    #62

--- includes/customizer/customizer.php
+++ includes/customizer/customizer.php
@@ -1,19 +1,47 @@
 <?php
 
 function gw_customize_register($wp_customize){
+    // Facebook link setting
     $wp_customize->add_setting( 'gw_social_media_facebook_setting' , array(
         'default'   => '',
-        'transport' => 'refresh',
     ) );
+
+    //Facebook Tooltip Setting
+    $wp_customize->add_setting( 'gw_social_media_facebook_tooltip_setting' , array(
+        'default'   => 'Facebook',
+    ) );
+
+     // twitter link setting
+     $wp_customize->add_setting( 'gw_social_media_twitter_setting' , array(
+        'default'   => '',
+    ) );
+
     
+    // Social Media Section
     $wp_customize->add_section( 'gw_social_media_section' , array(
         'title'      => 'Social Media',
         'priority'   => 30,
     ) );
 
+
+    // Facebook link input
     $wp_customize->add_control( 'gw_social_media_facebook', array(
         'label'      => 'Facebook Link',
         'section'    => 'gw_social_media_section',
         'settings'   => 'gw_social_media_facebook_setting',
+    ) );
+
+    // Facebook tooltip input
+    $wp_customize->add_control( 'gw_social_media_facebook_tooltip', array(
+        'label'      => 'Facebook tooltip',
+        'section'    => 'gw_social_media_section',
+        'settings'   => 'gw_social_media_facebook_tooltip_setting',
+    ) );
+
+    // twitter link input
+    $wp_customize->add_control( 'gw_social_media_twitter', array(
+        'label'      => 'Twitter Link',
+        'section'    => 'gw_social_media_section',
+        'settings'   => 'gw_social_media_twitter_setting',
     ) );
 }
